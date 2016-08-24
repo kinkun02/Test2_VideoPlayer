3ffc7bc3a2f19c432a120ccffd249a4b77a43a35
kinkunmo02 <930409150@qq.com>
2016-08-24 16:24:40 +0800

    add *

---
+++ UI_30(视频播放)/MMPViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMPViewController.h
+//  UI_30(视频播放)
+//
+//  Created by Ibokan on 16/7/12.
+//  Copyright © 2016年 king-mo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MMPViewController : ViewController
+
+@end
